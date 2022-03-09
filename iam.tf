ddc1fdbbf6814186fe3239f9de717e7b7e997888
Andrew Waite <aw@infosanity.co.uk>
2022-03-09 19:57:27 +0000

    fix: remove stale comment

--- iam.tf
+++ iam.tf
@@ -26,6 +26,4 @@ resource "aws_iam_role_policy" "ssm_managed_honeypot_role_policy" {
   name   = "ssm_managed_honeypot_role_policy"
   role   = aws_iam_role.ssm_managed_honeypot_role.id
   policy = data.template_file.iam_instance_profile.rendered
-  #policy = file("policy/ssm_instance_profile.json")
-
 }
